3cec974254fa41559ca80672c04d7a0c7c4bd334
AdamastorLinsFrancaNetto <adamastorfranca@hotmail.com>
2021-04-24 01:37:42 -0300

    24/04/21

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+from menu import Menu
+
+class Main:
+
+    def __init__(self, sizex, sizey, title):
+
+        self.window = pygame.display.set_mode([sizex, sizey])
+        self.title = pygame.display.set_caption(title)
+
+        self.menu = Menu()
+
+        self.loop = True
+
+    def draw(self):
+        if not self.menu.change_scene:
+            self.menu.draw(self.window)
+
+    def events(self):
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                self.loop = False
+            self.menu.events(events)
+
+    def update(self):
+        while self.loop:
+            self.draw()
+            self.events()
+            pygame.display.update()
+
+game = Main(1280, 960, "----- ACADEMIC JOURNEY -----")
+game.update()
