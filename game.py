338b0e33aa86f2766dd614df6e7f64df58a2ea8b
AdamastorLinsFrancaNetto <adamastorfranca@hotmail.com>
2021-04-25 00:31:19 -0300

    25/04/21

--- game.py
+++ game.py
@@ -6,13 +6,19 @@ class Game:
 
         self.bg = Obj("assets/espaco.png",0,0)
         self.bg2 = Obj("assets/espaco.png", 0, -960)
+        self.nave = Obj("assets/nave1.png",640,800)
         self.change_scene = False
 
     def draw(self,window):
         self.bg.drawing(window)
         self.bg2.drawing(window)
+        self.nave.drawing((window))
 
     def update(self):
+        self.move_bg()
+        self.nave.amin()
+
+    def move_bg(self):
         self.bg.sprite.rect[1] += 1
         self.bg2.sprite.rect[1] += 1
 
@@ -20,4 +26,3 @@ class Game:
             self.bg.sprite.rect[1] = 0
         if self.bg2.sprite.rect[1] >= 0:
             self.bg2.sprite.rect[1] = -960
-
--- obj.py
+++ obj.py
@@ -10,12 +10,17 @@ class Obj:
         self.sprite.rect = self.sprite.image.get_rect()
         self.sprite.rect[0] = x
         self.sprite.rect[1] = y
+        self.frame = 1
 
     def drawing(self,window):
         self.group.draw(window)
 
     def amin(self):
-        
+        self.frame += 1
+        if self.frame > 2:
+            self.frame = 1
+        self.sprite.image = pygame.image.load("assets/nave" + str(self.frame) + ".png")
+
 
 
 
